f9aeef4ea10947e11058e2b2126e53eb01388c6a
Anay Saha <anaysaha2005@gmail.com>
2023-08-14 12:48:31 +0530

    .

---
+++ JAVA/Trees.java
@@ -0,0 +1,84 @@
+public class Trees {
+}
+ class TreeBuild{
+    public static class Node {
+        public int data;
+        public  Node leftlink;
+        public   Node rightlink;
+        public  Node prelink;
+        public int count;
+        Node(int data) {
+            this.data = data;
+           leftlink = null;
+            rightlink = null;
+            prelink=null;
+            count=0;
+        }
+    }
+
+     public static Node pointer;
+     public  static Node root;
+
+        public void add(int data){
+            if(root==null) {
+                root=new Node(data);
+                pointer=root;
+            }
+            else if(data==-1){
+               if(pointer.count==0){
+                   pointer.leftlink=null;
+                   pointer.count++;
+               }
+               if(pointer.count==1){
+                   pointer.rightlink=null;
+                   pointer.count++;
+               }
+            }
+           else if(pointer.count==0)
+            {
+                pointer.count++;
+                Node temp =new Node(data);
+                pointer.leftlink=temp;
+                temp.prelink=pointer;
+                pointer=temp;
+
+            }
+           else if(pointer.count==1)
+            {
+                pointer.count++;
+                Node temp =new Node(data);
+                pointer.rightlink=temp;
+                temp.prelink=pointer;
+                pointer=temp;
+
+            }
+            if(pointer.count>1)
+            {
+                pointer=pointer.prelink;
+
+            }
+
+
+        }
+    }
+class treeBuildCheck{
+    public static void main(String[] args) {
+        TreeBuild t=new TreeBuild();
+        t.add(1);
+        t.add(2);
+        t.add(4);
+        t.add(-1);
+        t.add(-1);
+        t.add(5);
+        t.add(-1);
+        t.add(-1);
+        t.add(3);
+        t.add(-1);
+        t.add(6);
+        t.add(-1);
+        t.add(-1);
+
+
+
+    }
+}
