4ad9c55ffa6a3709b927ae9339de1bf55036a03a
Anay Saha <anaysaha2005@gmail.com>
2023-08-14 19:51:13 +0530

    .

--- JAVA/Trees.java
+++ JAVA/Trees.java
@@ -1,3 +1,5 @@
+import java.util.TreeSet;
+
 public class Trees {
 }
  class TreeBuild{
@@ -64,7 +66,7 @@ public class Trees {
 
     }
 
-class TreeBuildRecursive{
+ class TreeBuildRecursive{
     public static class Node {
         int data;
         Node left;
@@ -76,6 +78,7 @@ class TreeBuildRecursive{
             left = null;
         }
     }
+    static Node root=null;
       static int idx=-1;
       public Node TreeBuild(int[] a){
           idx++;
@@ -85,16 +88,60 @@ class TreeBuildRecursive{
           Node newNode= new Node(a[idx]);
           newNode.left= TreeBuild(a);
           newNode.right= TreeBuild(a);
+          root=newNode;
           return newNode;
       }
-    }
+
+      public void binaryTreePrintPreOrder(Node temp){
+          if(temp==null)
+              return;
+          System.out.print(temp.data+"  ");
+          binaryTreePrintPreOrder(temp.left);
+          binaryTreePrintPreOrder(temp.right);
+
+
+      }
+     public void binaryTreePrintInOrder(Node temp){
+         if(temp==null)
+             return;
+
+         binaryTreePrintInOrder(temp.left);
+         System.out.print(temp.data+"  ");
+         binaryTreePrintInOrder(temp.right);
+
+
+     }
+     public void binaryTreePrintPostOrder(Node temp){
+         if(temp==null)
+             return;
+         binaryTreePrintPostOrder(temp.left);
+         binaryTreePrintPostOrder(temp.right);
+         System.out.print(temp.data+"  ");
+     }
+     public void binaryTreePrintLevelOrder(Node temp){
+          if(temp==root){
+              System.out.println(root.data+"  ");}
+          if(temp==null){
+              return;}
+         if(temp.left!=null){
+             System.out.print(temp.left.data+"  ");}
+         if(temp.right!=null) {
+             System.out.print(temp.right.data+"  ");}
+
+         binaryTreePrintLevelOrder(temp.left);
+         binaryTreePrintLevelOrder(temp.right);
+
+     }
+
+
+ }
 
 class treeBuildCheck{
     public static void main(String[] args) {
         TreeBuildRecursive t=new TreeBuildRecursive();
-    int[] a ={1,2,4,-1,-1,3,-1,6,-1,6,-1,-1};
-    t.TreeBuild(a);
-
+    int[] a ={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+   TreeBuildRecursive.Node root= t.TreeBuild(a);
+        t.binaryTreePrintLevelOrder(root);
 
 
     }
